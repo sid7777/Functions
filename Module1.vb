51a64b91047a5a67af9f7fcb771a49896d48135c
Muhammad Sudais <36643283+sid7777@users.noreply.github.com>
2018-02-20 01:37:32 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x, y As Single
+        x = Console.ReadLine
+        y = x / 2
+        Console.WriteLine(y + 0.5)
+        Console.WriteLine(y - 0.5)
+        Console.ReadKey()
+    End Sub
+
+End Module
